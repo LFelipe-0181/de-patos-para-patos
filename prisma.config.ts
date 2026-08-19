372d0041440f00acca313e35841d34b258b07df5
Luis Felipe <luisfelipegodinho7@gmail.com>
2026-08-19 20:35:10 -0300

    fix: remove prisma.config

--- prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    url: "postgresql://postgres.xkamhtswnpdgcwmcjuyd:_4Li43QfyPyb3$b@aws-0-sa-east-1.pooler.supabase.com:5432/postgres",
-  },
-});
